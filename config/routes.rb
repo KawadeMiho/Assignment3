be485e174dfc7aafd005c84228f9b45b29d74e0c
EC2 Default User <115885521+KawadeMiho@users.noreply.github.com>
2022-12-26 11:35:51 +0000

    課題3

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+
+  root to:"homes#top"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books,only: [:create, :index, :show, :destroy,:edit]
+
+  get "/homes/about" => "homes#about", as: "about"
+
+  resources :users,only: [:index, :show, :edit, :update, :destroy]
+
+end
